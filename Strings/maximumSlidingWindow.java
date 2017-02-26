98f3c8134a7ca215bfb8c6fbfe1c0cca2797c0be
Karthik Narayanan <karthikn@ufl.edu>
2017-02-26 01:49:56 -0500

    String algos added

---
+++ Strings/maximumSlidingWindow.java
@@ -0,0 +1,33 @@
+//Maximum Sliding Window of size k
+
+import java.util.ArrayDeque;
+
+public class Problem {
+    
+    public static void main(String[] args) {       
+        int i, k = 3;
+        ArrayDeque<Integer> d = new ArrayDeque<>();
+        int a[] = { 3, 1, -1, -3, 5, 3, 6, 7 };
+        int[] o = new int[a.length-2];
+        for( i = 0; i < k-1; i++ )
+        {
+            while( !d.isEmpty() && a[i] > d.peekLast() )
+                d.removeLast();
+            d.addLast( a[i] );
+        }
+        int j = 0;
+        for( i = k-1; i < a.length; i++ )
+        {
+            while( !d.isEmpty() && a[i] > d.peekLast() )
+                d.removeLast();
+            d.addLast( a[i] );
+            
+            if( d.size() > k )
+                d.removeFirst();
+            
+            o[j++] = d.peekFirst();
+        }
+        for( i = 0; i < o.length; i++ )
+            System.out.println( o[i] + " " );
+    }
+}
