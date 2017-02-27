fe4334571dc38a21482e0f98676ca35eb12e9789
Karthik Narayanan <karthikn@ufl.edu>
2017-02-27 00:58:01 -0500

    Update TwoLinkedListOperations.java

--- LinkedList/TwoLinkedListOperations.java
+++ LinkedList/TwoLinkedListOperations.java
@@ -125,15 +125,15 @@ public class Problem {
         head2 = new Node( 6 );
         head2.next = new Node( 7 );
         head2.next.next = new Node( 8 );
-		head2.next.next.next = new Node( 9 );
-		head2.next.next.next.next = new Node( 3 );
-		head2.next.next.next.next.next = new Node( 4 );
-		head2.next.next.next.next.next.next = new Node( 5 );
+	head2.next.next.next = new Node( 9 );
+	head2.next.next.next.next = new Node( 3 );
+	head2.next.next.next.next.next = new Node( 4 );
+	head2.next.next.next.next.next.next = new Node( 5 );
         
         list.printList( head1 );
         list.printList( head2 );
 		
-		Node temp = list.intersectionList( head1, head2 );
+	Node temp = list.intersectionList( head1, head2 );
         if( temp != null )
             System.out.println( "Intersection point: " + temp.data );
         else
