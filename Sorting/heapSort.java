c3ac7142e31144cf33f611e692fb550bbc485139
Karthik Narayanan <karthikn@ufl.edu>
2017-04-02 19:44:16 -0400

    minor bug fix

--- Sorting/heapSort.java
+++ Sorting/heapSort.java
@@ -8,17 +8,17 @@ public class Problem {
     
     public static int parent( int i )
     {
-        return i/2;
+        return (i-1)/2;
     }
     
     public static int left( int i )
     {
-        return 2*i;
+        return (2*i)+2;
     }
     
     public static int right( int i )
     {
-        return (2*i)+1;
+        return (2*i)+2;
     }
     
     public static void maxHeapify( int a[], int i )
